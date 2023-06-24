87ee0aa12dcd595364721451c7787e6939b95ed1
Shrijan <shrijan.sk@gmail.com>
2023-06-24 13:22:53 +0530

    initial commit

---
+++ Lecture 9/find-maxelement/Main.java
@@ -0,0 +1,21 @@
+
+public class Main
+{
+   public static int findMaximum(int []arr, int index) {
+          if(index < 0) {
+          return Integer.MIN_VALUE;
+         }
+         int temp = findMaximum(arr,index - 1);
+         int max = Math.max (temp, arr[index]);
+         return max;
+}
+
+
+public static void main(String[] args) 
+{
+    int arr [] = {5,2,17, 13, 12, 15};
+    System.out.println(findMaximum(arr,arr.length - 1));
+	
+		
+	}
+}
